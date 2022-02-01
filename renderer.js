0b40b0bd55cb4db2009a138f63c00c53be6cfa7e
jweir136 <jweir136@yahoo.com>
2022-02-01 10:13:45 -0500

    import bodypix model and load img filepath as Image object

--- renderer.js
+++ renderer.js
@@ -1,9 +1,23 @@
 const extractFrames = require('ffmpeg-extract-frames')
 var fs = require('fs');
 var path = require('path');
+const bodyPix = require('@tensorflow-models/body-pix');
+const Canvas = require("canvas");
+const tf = require("@tensorflow/tfjs");
+
+let img = new Canvas.Image();
 
 async function main()
 {
+    let imgPath;
+    let net = await bodyPix.load({
+        architecture: 'MobileNetV1',
+        outputStride: 16,
+        multiplier: 0.75,
+        quantBytes: 2,
+        internalResolution: 'medium'
+    });
+
     await extractFrames({
         input: './IMG_1319.mov',
         output: 'media/screenshot-%d.jpg',
@@ -14,7 +28,10 @@ async function main()
     // NOTE: files are all out of order due to this function using async
     const dir = await fs.promises.opendir("media/");
     for await (const dirent of dir) {
-        console.log("media/" + dirent.name);
+        imgPath = "media/" + dirent.name; 
+
+        img.src = imgPath;
+        console.log(img);
     }  
 }
 
