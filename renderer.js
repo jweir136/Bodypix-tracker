e8334a72978d7b36c80523cd36896ebfdadc20cc
jweir136 <jweir136@yahoo.com>
2022-02-01 09:23:04 -0500

    add snippet to iterate through files

--- renderer.js
+++ renderer.js
@@ -1,12 +1,21 @@
 const extractFrames = require('ffmpeg-extract-frames')
+var fs = require('fs');
+var path = require('path');
 
 async function main()
 {
-await extractFrames({
-    input: './IMG_1319.mov',
-    output: './screenshot-%d.jpg',
-    fps: 160
-  })
+    await extractFrames({
+        input: './IMG_1319.mov',
+        output: 'media/screenshot-%d.jpg',
+        fps: 160
+    });
+
+    // ITERATE THROUGH ALL FILES
+    // NOTE: files are all out of order due to this function using async
+    const dir = await fs.promises.opendir("media/");
+    for await (const dirent of dir) {
+        console.log("media/" + dirent.name);
+    }  
 }
 
 main();
