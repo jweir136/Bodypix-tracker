a971bf4d33f48d3d2d694d6275d76b5d6348f9f4
jweir136 <jweir136@yahoo.com>
2022-02-02 16:09:58 -0500

    add relative parsing code

--- parser.js
+++ parser.js
@@ -10,8 +10,51 @@ function parse(filename, referencePoint1, referencePoint2)
         return JSON.parse(rawdata);
     }
 
-    let data = loadJSON(filename);
-    console.log(data);
+    function calculateDistance(X1, X2, Y1, Y2)
+    {
+        let deltaX = X1 - X2;
+        let deltaY = Y1 - Y2;
+
+        return Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+    }
+
+    function containsBothReferencePoints(data, referencePoint1, referencePoint2)
+    {
+        return data[referencePoint1] != undefined && data[referencePoint2] != undefined;
+    }
+
+    let data = loadJSON(filename)["data"];
+    let outputData = [];
+
+    let deltaReference;
+    let frame;
+
+    for (let i = 0; i < data.length; i+=1)
+    {
+        if (containsBothReferencePoints(data[i], referencePoint1, referencePoint2))
+        {
+            frame = {};
+
+            deltaReference = calculateDistance(data[i][referencePoint1]["x"], data[i][referencePoint2]["x"], data[i][referencePoint1]["y"], data[i][referencePoint2]["y"]);
+            frame["deltaReference"] = deltaReference;
+
+            for (bodyPart in data[i])
+            {
+                frame[bodyPart] = (
+                        calculateDistance(data[i][referencePoint1]["x"], data[i][bodyPart]["x"], data[i][referencePoint1]["y"], data[i][bodyPart]["y"])
+                    ) / deltaReference;
+            }
+
+            outputData.push(frame);
+        }
+        else
+        {
+            outputData.push(null);
+        }
+    }
+    
+    // TODO : save outputData as JSON
+
 }
 
-parse("output.json", null, null);
+parse("output.json", "leftEye", "rightEye");
