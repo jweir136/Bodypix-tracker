bc5081bc500180f250c0968cf0bf12d1e4f713a3
jweir136 <jweir136@yahoo.com>
2022-02-11 10:17:15 -0500

    init branch and add test #1

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-let { app, BrowserWindow } = require("electron");
+let DTW = require("dtw");
 
-function createWindow () {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 800
-    });
-  
-    win.loadFile('index.html');
+{
+    console.log("\n");
+    console.log("Test #1");
+    console.log("####################################");
+
+    let s = [1,1,2,3,2,0];
+    let t = [0,1,1,2,3,2,1];
+
+    let dtw = new DTW();
+
+    let cost = ((3 * t.length) - dtw.compute(s, t)) / (3 * t.length);
+
+    console.log("Simularity Score: " + cost);
+
+    console.log("####################################");
+    console.log("\n");
 }
-  
-  app.whenReady().then(createWindow);
