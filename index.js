ccce8f7c38028bb38708b9b8b9de4d24f22ae01c
jweir136 <jweir136@yahoo.com>
2022-02-11 10:19:08 -0500

    add better formatting for test #1

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let DTW = require("dtw");
     console.log("\n");
     console.log("Test #1");
     console.log("####################################");
+    console.log("\n");
 
     let s = [1,1,2,3,2,0];
     let t = [0,1,1,2,3,2,1];
@@ -14,6 +15,7 @@ let DTW = require("dtw");
 
     console.log("Simularity Score: " + cost);
 
+    console.log("\n");
     console.log("####################################");
     console.log("\n");
 }
